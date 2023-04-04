c8df42e4eccebe8aadb42c9862e6983a9cebf1a6
niel <niel.libunao@students.isatu.edu.ph>
2023-04-04 22:22:24 +0800

    added binarytree and location point

--- vanilla_code/index.php
+++ vanilla_code/index.php
@@ -8,43 +8,123 @@
     <meta https-equiv="X-UA-Compatible" content="ie=edge" />
     <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css" />
     <link rel="stylesheet" href="assets/vendor/bootstrap-icons/bootstrap-icons.css" />
+    <link rel="stylesheet" href="assets/vendor/fontawesome-free/css/all.min.css">
     <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <style>
     #layout-image {
+
         width: 100%;
+
     }
-    .inner-canvas{
+
+    .inner-canvas {
         padding: 400px;
     }
+
     body {
         overflow: hidden;
         /* Hide scrollbars */
     }
+
+
+    .office {
+        position: absolute;
+        cursor: pointer;
+        border: 2px solid black;
+        border-radius: 50%;
+        background-color: #99B898;
+        width: 80px;
+        height: 50px;
+        text-align: center;
+        line-height: 50px;
+        font-weight: bold;
+        font-size: 20px;
+    }
+
+    #office1 {
+        top: 600px;
+        left: 750px;
+    }
+
+    #office2 {
+        top: 800px;
+        left: 1200px;
+    }
+
+    #office3 {
+        top: 900px;
+        left: 1050px;
+    }
+
+    
+
 
     </style>
 </head>
 
 <body class="bg-secondary">
 
-        
+
+
+
     <div class="outer-canvas">
         <div id="zoom">
-                
+
             <div class="inner-canvas">
                 <img src="layout.png" alt="zoom" id="layout-image">
+                <div class="office" id="office1">KM</div>
+                <div class="office" id="office2">SUPPLY</div>
+                <div class="office" id="office3">AO</div>
+
+
+
             </div>
         </div>
-        
+
+
     </div>
 
-        <div class="hud-canvas fixed-top m-5 p-2 bg-primary rounded shadow" style="width: 100px;">
-            <button class="btn btn-light rounded mb-2" style="width: 85px;"> <strong>FLOOR</strong> <br> 0</button>
-            <button class="btn btn-light rounded mb-2" style="width: 85px;"> <strong>FLOOR</strong> <br> 1</button>
-            <button class="btn btn-light rounded mb-2" style="width: 85px;"> <strong>FLOOR</strong> <br> 2</button>
-            <button class="btn btn-light rounded mb-2" style="width: 85px;"> <strong>FLOOR</strong> <br> 3</button>
-            <button class="btn btn-light rounded" style="width: 85px;"> <strong><h1 class="mb-0"><i class="bi bi-zoom-in"></h1></i></strong></button>
-            <button class="btn btn-light rounded" style="width: 85px;"> <strong><h1 class="mb-0"><i class="bi bi-zoom-in"></h1></i></strong></button>
+
+    <div class="hud-canvas fixed-top m-5 p-2 bg-primary rounded shadow" style="width: 100px;">
+        <button class="btn btn-light rounded mb-2" style="width: 85px;"> <strong>FLOOR</strong> <br> 0</button>
+        <button class="btn btn-light rounded mb-2" style="width: 85px;"> <strong>FLOOR</strong> <br> 1</button>
+        <button class="btn btn-light rounded mb-2" style="width: 85px;"> <strong>FLOOR</strong> <br> 2</button>
+        <button class="btn btn-light rounded mb-2" style="width: 85px;"> <strong>FLOOR</strong> <br> 3</button>
+        <button class="btn btn-light rounded" style="width: 85px;"> <strong>
+                <h1 class="mb-0"><i class="bi bi-zoom-in"></h1></i>
+            </strong></button>
+        <button class="btn btn-light rounded" style="width: 85px;"> <strong>
+                <h1 class="mb-0"><i class="bi bi-zoom-in"></h1></i>
+            </strong></button>
+    </div>
+
+
+
+
+
+    </div>
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
         </div>
+    </div>
+
+
 
     <script>
     var scale = 1,
@@ -95,7 +175,13 @@
 
         setTransform();
     }
+
+    $('#exampleModal').on('shown.bs.modal', function() {
+        $('#modal').trigger('focus')
+    })
     </script>
+
+
 </body>
 
 </html>
