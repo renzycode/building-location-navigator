ff58c92c31851d8cb6a6d205c24fad5f62f1b9d3
renzycode <renzyjohnm1@gmail.com>
2023-06-12 11:28:13 +0800

    updated

--- edited/floor1.php
+++ edited/floor1.php
@@ -13,16 +13,54 @@
 </head>
 
 <body class="bg-secondary">
-
-    <?php include_once 'unitsData.php'; ?>
-
+    <?php
+        $units = array();
+        $units = [
+            [
+                'shortName'=>'KM',
+                'fullName'=>'Knowledge Management',
+                'rgbColor'=>'247, 0, 0',
+                'pinPointTopLocation'=>'285',
+                'pinPointLeftLocation'=>'620',
+                'pulseTopLocation'=>'255',
+                'pulseLeftLocation'=>'620',
+                'gotoLocation'=>'300,700,4',
+                'bounce'=>FALSE,
+                'pulse'=>FALSE
+            ],
+            [
+                'shortName'=>'HRMO',
+                'fullName'=>'Way ko balo',
+                'rgbColor'=>'200, 200, 0',
+                'pinPointTopLocation'=>'100',
+                'pinPointLeftLocation'=>'100',
+                'pulseTopLocation'=>'100',
+                'pulseLeftLocation'=>'100',
+                'gotoLocation'=>'100,600,4',
+                'bounce'=>FALSE,
+                'pulse'=>FALSE
+            ],
+            [
+                'shortName'=>'HFTC',
+                'fullName'=>'Knowledge Management',
+                'rgbColor'=>'247, 0, 155',
+                'pinPointTopLocation'=>'300',
+                'pinPointLeftLocation'=>'800',
+                'pulseTopLocation'=>'100',
+                'pulseLeftLocation'=>'500',
+                'gotoLocation'=>'340,800,4',
+                'bounce'=>FALSE,
+                'pulse'=>FALSE
+            ]
+        ];
+    ?>
     <div class="outer-canvas">
         <div id="zoom">
-            
+
             <!-- map canvas -->
             <div class="inner-canvas">
                 <img src="floor-images/1.png" alt="zoom" id="layout-image">
-                
+
                 <?php
                 foreach($units as $unit){
                     echo '
